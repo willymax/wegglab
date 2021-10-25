8ebc58518b087d78d7f5911efdfea6fe9d130836
Nishad Uzzaman <nishad@gmail.com>
2021-10-25 12:03:31 +0300

    Fixed ask question

--- pages/questions/ask.vue
+++ pages/questions/ask.vue
@@ -6,12 +6,22 @@
       <option>Physcis</option>
       <option>Science</option>
     </select>
+    <base-input
+      v-model="input.title"
+      label="Title"
+      placeholder="Enter Question Title"
+    />
+    <validation-error :errors="apiValidationErrors.title" />
     <textarea
+      v-model="input.body"
       class="form-textarea mt-1 block w-full p-2"
       rows="3"
       placeholder="Enter the description of your question."
     ></textarea>
+    {{ input }}
+    <validation-error :errors="apiValidationErrors.body" />
     <BaseFileUpload v-model="FILES"></BaseFileUpload>
+    <validation-error :errors="apiValidationErrors.email" />
     <base-button @click="postQuestion()">Submit</base-button>
   </div>
 </template>
@@ -19,15 +29,21 @@
 <script>
 import BaseButton from '~/components/core-components/BaseButton.vue'
 import BaseFileUpload from '~/components/core-components/BaseFileUpload.vue'
+import formMixin from '@/mixins/form-mixin'
 export default {
   components: {
     BaseFileUpload,
     BaseButton,
   },
+  mixins: [formMixin],
   layout: 'DashboardLayout',
   data() {
     return {
       FILES: {},
+      input: {
+        title: 'The title',
+        body: 'The body',
+      },
     }
   },
   watch: {
@@ -36,15 +52,38 @@ export default {
   methods: {
     postQuestion() {
       const formData = new FormData()
-      for (const key in this.FILES) {
-        const file = this.FILES[key]
-        formData.append(file.name, file)
+      const questionFiles = []
+      let counter = 0
+      for (const [index, file] of Object.entries(this.FILES)) {
+        formData.append(`questionFiles[${counter}]`, file)
+        counter++
       }
-      this.$axios.post('questions', formData, {
+      formData.append('title', this.input.title)
+      formData.append('body', this.input.body)
+      delete this.$axios.defaults.headers.common['content-type']
+      delete this.$axios.defaults.headers.post['content-type']
+      this.$axios({
+        method: 'POST',
+        url: 'questions',
+        data: formData,
         headers: {
           'Content-Type': 'multipart/form-data',
+          Accept: 'application/json',
         },
       })
+        .then((response) => {
+          this.$notify({
+            type: 'success',
+            message: 'Question created successfully.',
+          })
+        })
+        .catch((error) => {
+          console.log(JSON.stringify(error.response.data))
+          this.setApiValidation(error.response.data.errors)
+        })
+        .then(function () {
+          // always executed
+        })
     },
   },
 }
--- plugins/dashboard/JsonApi.js
+++ plugins/dashboard/JsonApi.js
@@ -1,6 +1,6 @@
 // thanks to https://github.com/mrichar1/jsonapi-vuex/issues/13
 export default function ({ $axios, store, redirect }) {
-  $axios.setHeader('content-type', 'application/vnd.api+json')
+  // $axios.setHeader('content-type', 'application/vnd.api+json')
   $axios.onRequest((config) => {
     console.log('Making request to ' + config.url)
   })
@@ -10,5 +10,9 @@ export default function ({ $axios, store, redirect }) {
     if (code === 400) {
       redirect('/400')
     }
+  })
+  $axios.interceptors.request.use((request) => {
+    console.log('Starting Request', JSON.stringify(request, null, 2))
+    return request
   })
 }
