0d07b0ce5bf6e8aeac56fa74740998127429af9f
William Makau <williammakau070@gmail.com>
2022-03-08 12:58:17 +0300

    Before updating to newer tailwind

--- components/answers/BlurredAnswer.vue
+++ components/answers/BlurredAnswer.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="relative w-full h-44">
+  <div class="relative w-full h-40">
     <div class="top-0 left-0 absolute w-full h-full">
-      <img src="~/assets/img/blurry.png" class="object-cover h-48 w-full" />
+      <img src="~/assets/img/blurry.png" class="object-cover h-full w-full" />
     </div>
-    <div class="top-0 left-0 z-10 absolute w-full h-full">
-      <card class="w-1/3 text-center m-auto bg-gray-300">
+    <div class="top-0 left-0 bottom-0 absolute w-full h-full">
+      <card class="w-1/3 object-center text-center m-auto bg-gray-300">
         <h2>Subscribe to access answers</h2>
         <base-nuxt-button-link to="/subscription/plans"
           >Subscribe</base-nuxt-button-link
