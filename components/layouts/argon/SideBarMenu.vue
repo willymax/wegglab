076ddeaf9951d9834dbb5ae48ddab3531d64f0e8
William Makau <williammakau070@gmail.com>
2022-05-06 08:22:03 +0300

    ui fix

--- components/layouts/argon/SideBarMenu.vue
+++ components/layouts/argon/SideBarMenu.vue
@@ -4,43 +4,59 @@
     aria-label="Sidebar"
     class="flex-1 px-2 py-4 space-y-2 overflow-y-hidden hover:overflow-y-auto text-base-content"
   >
-    <side-bar-menu-link>
-      <template #link-icon>
-        <img class="w-5 h-5" src="~assets/wallet.svg" />
-      </template>
-      Subscriptions
-      <template #menus>
-        <menu-item to="/plans">List Plans</menu-item>
-        <menu-item to="/products">List Products</menu-item>
-        <menu-item to="/plans/create">Create Plan</menu-item>
-        <menu-item to="/products/create">Create Product</menu-item>
-        <menu-item to="/subscriptions/plans">Subscribe</menu-item>
-        <menu-item to="/subscriptions">Subscriptions</menu-item>
-      </template>
-    </side-bar-menu-link>
-    <menu-item to="/questions">
-      <template #link-icon>
-        <img class="w-5 h-5 inline" src="~assets/question-mark.svg" />
-      </template>
-      Questions</menu-item
-    >
-    <menu-item to="/subjects">
-      <template #link-icon>
-        <img class="w-5 h-5 inline" src="~assets/book.svg" />
-      </template>
-      Subjects</menu-item
-    >
-    <side-bar-menu-link>
-      <template #link-icon>
-        <img class="w-5 h-5" src="~assets/img/user.png" />
-      </template>
-      Users
-      <template #menus>
-        <menu-item to="/users">Users (All for now)</menu-item>
-        <menu-item to="/experts">Experts</menu-item>
-        <menu-item to="/students">Students</menu-item>
-      </template>
-    </side-bar-menu-link>
+    <div v-if="$auth.loggedIn">
+      <side-bar-menu-link>
+        <template #link-icon>
+          <img class="w-5 h-5" src="~assets/wallet.svg" />
+        </template>
+        Subscriptions
+        <template #menus>
+          <menu-item to="/plans">List Plans</menu-item>
+          <menu-item to="/products">List Products</menu-item>
+          <menu-item to="/plans/create">Create Plan</menu-item>
+          <menu-item to="/products/create">Create Product</menu-item>
+          <menu-item to="/subscriptions/plans">Subscribe</menu-item>
+          <menu-item to="/subscriptions">Subscriptions</menu-item>
+        </template>
+      </side-bar-menu-link>
+      <menu-item to="/questions">
+        <template #link-icon>
+          <img class="w-5 h-5 inline" src="~assets/question-mark.svg" />
+        </template>
+        Questions</menu-item
+      >
+      <menu-item to="/subjects">
+        <template #link-icon>
+          <img class="w-5 h-5 inline" src="~assets/book.svg" />
+        </template>
+        Subjects</menu-item
+      >
+      <side-bar-menu-link>
+        <template #link-icon>
+          <img class="w-5 h-5" src="~assets/img/user.png" />
+        </template>
+        Users
+        <template #menus>
+          <menu-item to="/users">Users (All for now)</menu-item>
+          <menu-item to="/experts">Experts</menu-item>
+          <menu-item to="/students">Students</menu-item>
+        </template>
+      </side-bar-menu-link>
+    </div>
+    <div>
+      <menu-item to="/subjects">
+        <template #link-icon>
+          <img class="w-5 h-5 inline" src="~assets/book.svg" />
+        </template>
+        Physics</menu-item
+      >
+      <menu-item to="/subjects">
+        <template #link-icon>
+          <img class="w-5 h-5 inline" src="~assets/book.svg" />
+        </template>
+        Chemistry</menu-item
+      >
+    </div>
     <!-- Test Sidebar Menu links -->
     <slot></slot>
   </nav>
