c76abd30cd892900bff1947321ad1f3590ca1d0c
William Makau <williammakau070@gmail.com>
2022-06-24 07:32:31 +0300

    exclude button on links

--- components/core-components/BaseNuxtButtonLink.vue
+++ components/core-components/BaseNuxtButtonLink.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
     :to="to"
-    class="inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
+    class="button inline-block px-6 py-2.5 bg-green-500 text-white font-medium text-xs leading-tight uppercase rounded-full shadow-md hover:bg-green-600 hover:shadow-lg focus:bg-green-600 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-green-700 active:shadow-lg transition duration-150 ease-in-out"
   >
     <slot></slot>
   </NuxtLink>
